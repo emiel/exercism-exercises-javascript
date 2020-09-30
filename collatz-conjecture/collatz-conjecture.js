fa498f8ff6547a505f2d63fdca9d118b90154c7f
Emiel van de Laar <gemiel@gmail.com>
2020-09-30 16:05:23 +0200

    Exercise: Collatz Conjecture

---
+++ collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const steps = (n) => {
+  if (n <= 0) {
+    throw Error("Only positive numbers are allowed");
+  }
+
+  let iter = 0;
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
+    }
+    iter += 1;
+  }
+
+  return iter;
+};
