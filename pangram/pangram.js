ffa92cb031a0076ea9453df4278ccd559a97e2b8
Emiel van de Laar <gemiel@gmail.com>
2020-10-14 10:55:30 +0200

    Exercise: Pangram (light)

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,23 +1,18 @@
 export const isPangram = (sentence) => {
-  // Allocate a array which contains one item per letter in the alphabet. When
-  // the letter is seen in the sentence the relevant position is set to one.
-  //
-  // [0,0,0  ...0]
-  //  0,1,2  ...25
-  //  a,b,c  ...z
-  const letterFlags = new Uint8Array(26);
+  // The lower 26 bits of `seen` indicate if the corresponding letter is
+  // present in the sentence.
+  let seen = 0;
 
   for (let i = 0; i < sentence.length; i++) {
     const code = sentence.charCodeAt(i);
     if (code >= 65 && code <= 90) {
       // Uppercase letters
-      letterFlags[code - 65] = 1;
+      seen |= 1 << (code - 65);
     } else if (code >= 97 && code <= 122) {
       // Lowercase letters
-      letterFlags[code - 97] = 1;
+      seen |= 1 << (code - 97);
     }
   }
 
-  // If every position (letter) has been set we have a pangram.
-  return letterFlags.every(Boolean);
+  return seen === 0x3ffffff;
 };
