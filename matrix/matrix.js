a177546bf062ba76a67a7524fadd14ab7b611dd3
Emiel van de Laar <gemiel@gmail.com>
2021-01-07 10:24:09 +0100

    matrix: Quick caching implementation

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -3,6 +3,8 @@ export class Matrix {
     this.matrix = input
       .split("\n")
       .map((line) => line.split(" ").map((n) => Number(n)));
+
+    this.columnsCache = undefined;
   }
 
   get rows() {
@@ -10,22 +12,27 @@ export class Matrix {
   }
 
   get columns() {
-    const result = [];
+    if (this.columnsCache !== undefined) {
+      return this.columnsCache;
+    } else {
+      const result = [];
 
-    let colCount = 0;
-    if (this.rows.length > 0 && this.rows[0].length > 0) {
-      colCount = this.rows[0].length;
-    }
-
-    // Iterate over matrix in column major order
-    for (let i = 0; i < colCount; i++) {
-      let col = [];
-      for (let j = 0; j < this.rows.length; j++) {
-        col.push(this.rows[j][i]);
+      let colCount = 0;
+      if (this.rows.length > 0 && this.rows[0].length > 0) {
+        colCount = this.rows[0].length;
       }
-      result.push(col);
-    }
 
-    return result;
+      // Iterate over matrix in column major order
+      for (let i = 0; i < colCount; i++) {
+        let col = [];
+        for (let j = 0; j < this.rows.length; j++) {
+          col.push(this.rows[j][i]);
+        }
+        result.push(col);
+      }
+
+      this.columnsCache = result;
+      return this.columnsCache;
+    }
   }
 }
